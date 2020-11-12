ecc32487a4b2c108ae8a050b84df021e67c093c6
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-12 18:45:17 +0800

    completed practice 1 and 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = (input) => {
+    let result = [];
+    input.data.map((key) => {
+        let parse = {};
+        input.column.map((columnKey,columnValue) => {
+            parse[columnKey.name] = key[columnValue];
+        });
+        result.push(parse);
+    });
+    return result;
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+    let result = [...items];
+    sections.map((i) => {
+        i.index===0 ? result.splice(i.index, 0, i.content) : result.splice(i.index+1, 0, i.content);
+    });
+    return result;
+}
+
 export { inject };
